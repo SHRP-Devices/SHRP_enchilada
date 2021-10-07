8a84f8a50e8168f6ba8a2935e07642ccecb79260
Siddhesh Koyande <siddhrsh@users.noreply.github.com>
2021-10-07 22:34:55 +0530

    OOS11

--- omni_enchilada.mk
+++ omni_enchilada.mk
@@ -1,11 +1,13 @@
 #
-# Copyright 2017 The Android Open Source Project
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,14 +18,18 @@
 
 # Release name
 PRODUCT_RELEASE_NAME := enchilada
+DEVICE_PATH := device/oneplus/enchilada
 
-$(call inherit-product, build/target/product/embedded.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit from our custom product configuration
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oneplus/enchilada/device.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := enchilada
@@ -35,7 +41,8 @@ PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=oneplus6 \
     BUILD_PRODUCT=OnePlus6 \
-    TARGET_DEVICE=OnePlus6
+    TARGET_DEVICE=OnePlus6 \
+	PRODUCT_DEVICE=OnePlus6
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
